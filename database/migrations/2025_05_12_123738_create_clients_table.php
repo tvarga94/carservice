018381c8759dee0febe78cc5e45e1767f773e351
Varga Tamas <vargata1@gmail.com>
2025-05-12 14:40:41 +0200

    database structure

--- database/migrations/2025_05_12_123738_create_clients_table.php
+++ database/migrations/2025_05_12_123738_create_clients_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('card_number')->unique();
             $table->timestamps();
         });
     }
--- database/migrations/2025_05_12_123742_create_cars_table.php
+++ database/migrations/2025_05_12_123742_create_cars_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+            $table->unsignedInteger('car_id'); // unique per client
+            $table->string('type');
+            $table->date('registered');
+            $table->boolean('ownbrand');
+            $table->unsignedInteger('accidents');
             $table->timestamps();
+
+            $table->unique(['client_id', 'car_id']); // enforce uniqueness per client
         });
     }
 
